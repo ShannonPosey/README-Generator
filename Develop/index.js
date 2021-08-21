bd7aee7b396751cd5aea6cb2d5c26358ebc905fc
shannon_p <snp1284kasey@aol.com>
2021-08-21 15:18:40 -0400

    added licenses and badges

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "What licenses should be included in your project",
+        choices: ["MIT", "GNU", "Boost Software License 1.0", "Apache License 2.0", "Mozilla Public License 2.0", "Unlicense"]
+    },
+    {
+        type: "",
+        name: "",
+        message: ""
+    },
+];
+
+const promptUser = () => {
+    return inquirer
+    .prompt(questions);
+};
+// What is the name of your project?
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fileName
+}
+
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//     Name: ${userName}
+//     GitHub: ${githubName}
+//     `;
+// };
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,10 +1,24 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  "MIT": "[![License: MIT] (https://img.sheields.io/badge/License-MIT-blue)]",
+  "GNU": "[![License: GNU] (https://img.shields.io/badge/License-GNU-green)]",
+  "Boost Software License 1.0":"[![License: BSL 1.0] (https://img.shields.io/badge/License-BoostSoftwareLicense-purple)]",
+  "Mozilla Public License 2.0":"[![License: MPL 2.0] (https://img.shields.io/badge/License-MozillaPublicLicense2.0-orange)]",
+  "Apache License 2.0":"[![License: Apache License 2.0] (https://img.shields.io/badge/License-ApacheLicense2.0-red)]",
+  "Unlicense":"[![License: Unlicense] (https://img.shields.io/badge/License-Unlicense-yellow)]",
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  "MIT": "[![License: MIT] (https://opensource.org/licenses/MIT)",
+  "GNU": "[![License: GNU] (https://opensource.org/licenses/GPL-3.0)]",
+  "Mozilla Public License 2.0": "[![License: MPL 2.0] (https://opensource.org/licenses/MPL-2.0)]",
+  "Apache License 2.0": "[![License: Apache License 2.0] (https://opensource.org/licenses/Apache-2.0)]"
+  "Boost Software License 1.0": "[![License: BSL 1.0] (https://opensource.org/licenses/BSL-1.0)]"
+  "Unlicense": "[![License: Unlicense] (https://unlicense.org/)]"
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,6 +28,55 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description 
+${data.description}
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+* [Badges](#badges)
+* [Features](#feature)
+* [Contributing](#contributing)
+* [Tests](#test)
+
+## Installation
+
+What are the steps required to install your project?
+
+\`
+${data.installation}
+\`
+
+## Usage
+
+${data.usage}
+
+## Credits
+
+${data.credits}
+
+## License
+
+${data.license}
+
+## Badges
+
+${data.badges}
+
+## Features
+
+${data.features}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.test}
 `;
 }
 
